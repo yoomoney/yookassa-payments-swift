d522afdbf6c35c2e04e7f42ec445e74393addfed
Александр А. Золотарев <aazolotarev@yamoney.ru>
2018-10-09 17:56:51 +0300

    [BIOS-574] Fixed frieze in example app at iOS 8

--- YandexCheckoutPaymentsExample/AppDelegate.swift
+++ YandexCheckoutPaymentsExample/AppDelegate.swift
@@ -4,11 +4,12 @@ import YandexCheckoutPayments
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow? = UIWindow()
+    var window: UIWindow?
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+        window = UIWindow(frame: UIScreen.main.bounds)
         let viewController = UINavigationController(rootViewController: RootViewController())
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
